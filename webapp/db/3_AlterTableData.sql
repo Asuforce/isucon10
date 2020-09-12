e8c052d10ed644a0a8684cd46557ec2460170fdc
asuforce <owata.sn@gmail.com>
2020-09-12 17:12:35 +0900

    More simple

--- webapp/db/3_AlterTableData.sql
+++ webapp/db/3_AlterTableData.sql
@@ -1,3 +1,3 @@
-ALTER TABLE chair ADD INDEX index_chair_on_price(price);
-ALTER TABLE chair ADD INDEX index_chair_on_kind_on_stock(kind, stock);
-ALTER TABLE estate ADD INDEX index_estate_on_rent(rent);
+ALTER TABLE chair ADD INDEX index_price(price);
+ALTER TABLE chair ADD INDEX index_kind_stock(kind, stock);
+ALTER TABLE estate ADD INDEX index_rent(rent);
